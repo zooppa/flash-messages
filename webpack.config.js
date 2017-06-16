87432a6b338547d4fbd189b8d429d1cafab74737
Marco Arduini <mar.arduini@gmail.com>
2017-06-16 09:34:07 +0200

    Set up build and test scripts

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'FlashMessages',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  externals: [
+    nodeExternals(),
+  ], // do not bundle any node_modules
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-2'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};

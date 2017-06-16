56a95a5779ea88445f92c3c982e73c515aecd0d8
Marco Arduini <mar.arduini@gmail.com>
2017-06-16 11:17:38 +0200

    Set up index exports

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import flashMessagesReducer from './reducers/flashMessagesReducer';
+
+export {
+  receiveFlashMessage,
+  removeFlashMessage,
+  clearFlashMessages,
+  receiveTimedFlashMessage,
+} from './actions/flashMessages';
+
+export { flashMessagesReducer };

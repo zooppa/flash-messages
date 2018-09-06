10706e84cfc7638ed81b0a32613fc66940213abe
Marco Arduini <mar.arduini@gmail.com>
2018-09-06 15:23:04 +0200

    Bump babel-loader to 8.x, babel to 7.x

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,12 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react', 'stage-2'],
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
     ],
